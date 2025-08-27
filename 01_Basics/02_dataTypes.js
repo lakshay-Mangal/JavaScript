73a4287c65907e64f386e441b321f74fec044ad3
Lakshay Mangal <149657239+lakshay-Mangal@users.noreply.github.com>
2025-08-27 10:01:39 +0000

    DataTypes in JS

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"Use strict" //Treat all JS code as newer version
+
+
+// alert(3+3) we are using node.js so it won't work
+let name = "Lakshay"
+let age= 20
+let isLoggedIn= true
+let state;
+
+//number
+// string
+//boolean
+//BigInt
+//symbol
+//null
+//undefined
+console.log(typeof null) //null ka datatype object h jo ki language ki ek khaami h
+console.log(typeof age)
+console.log(typeof undefined)
