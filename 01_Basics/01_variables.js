8181605ce8159c5a43bf81ac886c8c984f150c84
Lakshay Mangal <149657239+lakshay-Mangal@users.noreply.github.com>
2025-08-27 08:23:58 +0000

    Basic variables in JS

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId= 1443
+let accountEmail= "xyz@gmail.com"
+var password= "!23232"
+accountCity= "Jaipur"
+let accountState;
+//accountId =2 not allowed
+console.log(accountId);
+
+accountEmail ="xyz@yahoo.com"
+console.table([accountId, accountEmail, accountCity, accountState])
