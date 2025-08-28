63a3052840db1e2aed6bece26e568a9431f68bc3
Lakshay Mangal <149657239+lakshay-Mangal@users.noreply.github.com>
2025-08-28 06:15:39 +0000

    conversions

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+let score= "33S"
+let score2= null
+
+// console.log(typeof score)
+// let valInNuM= Number(score);
+// console.log( valInNuM)
+// console.log(score2)
+
+let num="x"
+let booleanNum= Boolean(num)
+console.log(booleanNum)
+
+// OPERATIONS
+let num1=3
+let num2=3
+
+// console.log(num1+num2)
+// console.log(num1-num2)
+// console.log(num1*num2)
+// console.log(num1**num2)
+// console.log(num1/num2)
+// console.log(num1%num2)
+
+let str1= "Hello"
+let str2= "Yellow"
+let str3= str1+" "+str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"3")
+console.log(1+2+"3")
+console.log(+true)  //true+ is false;
+console.log(+"") 
