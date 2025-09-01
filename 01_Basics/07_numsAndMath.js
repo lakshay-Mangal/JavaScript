2278409d3c6ede410b02f7b4f9131c265c1b4ab4
Lakshay Mangal <149657239+lakshay-Mangal@users.noreply.github.com>
2025-09-01 08:27:16 +0000

    Math and number in codespace

---
+++ 01_Basics/07_numsAndMath.js
@@ -0,0 +1,23 @@
+const score =400
+
+const balance = new Number(100.43343)
+console.log(balance.toString().length)
+console.log(balance.toFixed(2));
+
+const num2= 324.6415326
+console.log(num2.toPrecision(3));
+const hundreds =100000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+//**********************************MATHS**************************
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.round(4.6));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,5,6,7,8,9,-4,-5,-6));
+console.log(Math.random()*10+1); 
+const min = 30
+const max= 60
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
