ca5d18983fe6050c11aab8cfa30e6cd512585e84
Lakshay Mangal <149657239+lakshay-Mangal@users.noreply.github.com>
2025-09-11 05:28:27 +0000

    More about scopes and hoisting

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -13,3 +13,40 @@ const b=20
 
  console.log(a);
 // console.log(b);
+
+function one (){
+   const username ="Lakshay"
+
+   function two(){
+      const website= "youtube"
+      console.log(username);
+      
+   }
+   // console.log(website);
+   two()
+}
+one();
+
+if(true){
+   const username="Lakshay"
+   if(username){
+      const website="youtube"
+      console.log(username+website) ;
+      
+   }
+   // console.log(website);
+   
+}
+// console.log(username);
+
+/* -------------------INTERSTING -------------- */
+console.log((addone(5))
+);
+function addone(num) {
+   return num+1;
+}
+//One to declaration se phle use ho rha h but add2 declaration se phle use nhi kar sakte
+addTwo(5)
+const addTwo= function(num){
+   return num+2;
+}
