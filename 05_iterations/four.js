c68c4d24eb233d439f06c7bcac0a4f8f38d3d989
Lakshay Mangal <149657239+lakshay-Mangal@users.noreply.github.com>
2025-09-12 11:10:39 +0000

    for in loop and its working

---
+++ 05_iterations/four.js
@@ -0,0 +1,16 @@
+const myObject = {
+    js : "javaScript",
+    cpp : "C++",
+    rb : "ruby",
+    swift: "Swift by apple"
+}
+
+
+for(const key in myObject) console.log(key,"->",myObject[key]);
+
+const programming = ["js","ruby","java","py","cpp"];
+
+for(const key in programming) console.log(programming[key]);
+
+
+
