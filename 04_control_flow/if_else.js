286f4897d30a82e1ba5edbfa1946779668a9fd54
Lakshay Mangal <149657239+lakshay-Mangal@users.noreply.github.com>
2025-09-12 05:27:11 +0000

    Working of if_else

---
+++ 04_control_flow/if_else.js
@@ -0,0 +1,15 @@
+const temp =41;
+
+if(temp>50) console.log("Temperature greater than 50");
+else console.log("Temperature less than 50");
+
+const balance =1000
+
+if(balance>500) console.log("test"), console.log("test"); //Bad practice do not do this by using comma
+const loggedIn= true;
+const debitCard= true;
+
+
+if(loggedIn && debitCard) console.log("Allow to buy courses");
+else console.log("can't buy courses");
+
