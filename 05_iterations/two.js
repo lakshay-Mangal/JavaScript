f44f91c536e1e3b718f9af330b25efc516908558
Lakshay Mangal <149657239+lakshay-Mangal@users.noreply.github.com>
2025-09-12 09:59:43 +0000

    While as well as do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+let i=0
+while (i<10) {
+    console.log(`HellO WORLD ${i}`);
+    i++;
+    
+}
+
+
+let myArray= ['Flash','witcher','batman'];
+
+let arr =0;
+while(arr<myArray.length) console.log(`value is ${myArray[arr]}`), arr++;
+
+let score =1;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=0);
