ef2f2d976a870b96e3d4f23f3f6613763e040f62
Lakshay Mangal <149657239+lakshay-Mangal@users.noreply.github.com>
2025-09-22 05:35:47 +0000

    How to change properties of object

---
+++ 10_OOP/mathPi.js
@@ -0,0 +1,33 @@
+const descriptor =Object.getOwnPropertyDescriptor(Math,'PI')
+
+console.log(Math.PI);
+Math.PI=5;
+console.log(Math.PI);
+
+console.log(descriptor);
+
+const chai = {
+    name: "ginger tea",
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("chai fat gyi");
+        
+    }
+}
+//if we false enumeration then it will not show up in loops
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+Object.defineProperty(chai, 'name',{
+    writable:false,
+    enumerable:false
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+ 
+for (let [key,value] of Object.entries(chai) ) {
+    if(typeof value != `function`)
+   { console.log(`${key} : ${value}`);}
+    
+}
